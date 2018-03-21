5946edc8cc76bd4617554609276efe2de066c001
Justyna Rachowicz <mrowa44@gmail.com>
2018-03-21 11:10:15 +0100

    Add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { MessageService } from './message.service';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [
     HeroService,
