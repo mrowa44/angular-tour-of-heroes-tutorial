1c7b9303a8a47065103b4f37855a4dfdb2ba3ed7
Justyna Rachowicz <mrowa44@gmail.com>
2018-03-21 11:45:27 +0100

    Hero add

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';
@@ -34,14 +38,19 @@ export class HeroService {
   }
 
   updateHero (hero: Hero): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    };
     return this.http.put(this.heroesUrl, hero, httpOptions)
       .pipe(
         tap(_ => this.log(`updated hero id=${hero.id}`)),
         catchError(this.handleError<any>('updateHero'))
       );
+  }
+
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+    .pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 
   private log(message: string) {
